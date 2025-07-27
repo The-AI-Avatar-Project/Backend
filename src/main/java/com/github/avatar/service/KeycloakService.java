8677b01e02a9624682dc2fff127b5005b95e5c73
KoanDev <okan.biyikli@hotmail.de>
2025-07-27 18:00:40 +0200

    Add Creator in created Room

--- src/main/java/com/github/avatar/service/KeycloakService.java
+++ src/main/java/com/github/avatar/service/KeycloakService.java
@@ -107,12 +107,24 @@ public class KeycloakService {
                 .block();
     }
 
-    public void createRoom(Jwt jwt, RoomCreationDTO roomDTO) {
-        Map<String, List<String>> attributes = new HashMap<>();
-        attributes.put("owner", List.of(jwt.getSubject()));
-        attributes.put("icon", List.of(roomDTO.icon()));
-        createRoomPath(roomDTO.year(), roomDTO.semester(), jwt.getClaim("family_name"), roomDTO.name(), attributes);
-    }
+public RoomDTO createRoom(Jwt jwt, RoomCreationDTO roomDTO) {
+    Map<String, List<String>> attributes = new HashMap<>();
+    attributes.put("owner", List.of(jwt.getSubject()));
+    attributes.put("icon", List.of(roomDTO.icon()));
+
+    RoomDTO room = createRoomPath(
+        roomDTO.year(),
+        roomDTO.semester(),
+        jwt.getClaim("family_name"),
+        roomDTO.name(),
+        attributes
+    );
+
+    // Add the creator as a member of the group
+    addUserToGroup(room.id(), jwt.getSubject());
+
+    return room;
+}
 
     private RoomDTO fetchGroupInfo(String path) {
         String token = getAdminAccessToken();
